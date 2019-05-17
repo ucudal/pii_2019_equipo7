bd7260eb4558849ae6cdaa89fb108ea0a7285b4a
RafaxMg <48605323+RafaxMg@users.noreply.github.com>
2019-05-17 19:44:58 -0300

    Clase Propuesta

---
+++ IgnisMercado/Models/Propuesta.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IgnisMercado.Models
+{
+    public class Propuesta
+    {
+        public string NivelDeDificultad { get; set; }
+        public string EstimadoPorHora { get; set; }
+        public string Estado{ get; set; }
+        public string DescripcionDeLaPropuesta { get; set; }        
+        public string ListaDeTecnicosPostulados { get; set; }
+        public string TecnicoAsignado { get; set; }
+
+    }
+}
