8c776c9932f8e1f13bdc9d220c3d2cb0dff28fc1
Michael <michael.romero@kadabrait.com>
2019-05-31 19:20:45 -0300

    clase propuesta

---
+++ IgnisMercado/Pages/Propuestas/Create.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using IgnisMercado.Models;
+
+namespace IgnisMercado.Pages.Propuestas
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IgnisMercado.Models.RazorPagesPropuestaContext _context;
+
+        public CreateModel(IgnisMercado.Models.RazorPagesPropuestaContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Propuesta Propuesta { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Propuesta.Add(Propuesta);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
