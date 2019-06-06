8d5541860f24a33f11b22fc2637ab154d1a9e074
Santiago Varaldo <santivaraldo02@gmail.com>
2019-06-06 01:04:05 -0300

    Modelos agregados Fedbacks y Competencias

---
+++ IgnisMercado/Pages/Competencias/Create.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using IgnisMercado.Models;
+
+namespace RazorPagesMovie.Pages.Competencias
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IgnisMercado.Models.IgnisContext _context;
+
+        public CreateModel(IgnisMercado.Models.IgnisContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Competencia Competencia { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Competencia.Add(Competencia);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ IgnisMercado/Pages/Competencias/Delete.cshtml.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using IgnisMercado.Models;
+
+namespace RazorPagesMovie.Pages.Competencias
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IgnisMercado.Models.IgnisContext _context;
+
+        public DeleteModel(IgnisMercado.Models.IgnisContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Competencia Competencia { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Competencia = await _context.Competencia.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Competencia == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Competencia = await _context.Competencia.FindAsync(id);
+
+            if (Competencia != null)
+            {
+                _context.Competencia.Remove(Competencia);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
