b6ddac9b204a2444b6434202d773ace62dba10d5
Michael <michael.romero@kadabrait.com>
2019-06-09 20:38:34 -0300

    se agrega clase ignis data y se mejoran los modelos

---
+++ IgnisMercado/Model/IginsData.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace IgnisMercado.Data
+{
+    public static class IgnisData
+    {
+        public const int PrecioHoraAvanzado = 150;
+
+        public const int PrecioHoraBasico = 150;
+    
+        public static string[] NonAdminRoleNames = new string[] { "Basico", "Basico" };
+    
+    }
+
+}
--- IgnisMercado/Model/Propuesta.cs
+++ IgnisMercado/Model/Propuesta.cs
@@ -6,14 +6,21 @@ namespace IgnisMercado.Models
 {
     public class Propuesta
     {
+        [Key]
         public  int ID { get; set; }
+        [Display(Name = "Title")]
         public string Title { get; set; }
+        [Display(Name = "Nivel De Dificultad")]
         public string NivelDeDificultad { get; set; }
-       
+       [Display(Name = "Estimado Por Hora")]
         public string EstimadoPorHora { get; set; }
+        [Display(Name = "Title")]
         public string Estado{ get; set; }
-        public string DescripcionDeLaPropuesta { get; set; }        
+        [Display(Name = "Title")]
+        public string DescripcionDeLaPropuesta { get; set; }    
+        [Display(Name = "Title")]    
         public string ListaDeTecnicosPostulados { get; set; }
+        [Display(Name = "Title")]
         public string TecnicoAsignado { get; set; }
 
     }
