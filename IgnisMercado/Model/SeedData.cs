638a7c46237e36de6ab9bfba11a9ab2f3cd492d8
RafaxMg <48605323+RafaxMg@users.noreply.github.com>
2019-07-02 19:07:50 -0300

    Seeding de Empresas en la DB

--- IgnisMercado/Model/SeedData.cs
+++ IgnisMercado/Model/SeedData.cs
@@ -15,9 +15,9 @@ namespace IgnisMercado.Models
                     DbContextOptions<ApplicationContext>>()))
             {
                 SeedPropuesta(context);
-
+                SeedEmpresa(context);
             }
-         }
+        }
         private static void SeedPropuesta(ApplicationContext context)
         {
             // Busca Propuestas.
@@ -26,9 +26,9 @@ namespace IgnisMercado.Models
                 return;   // DB ha sido poblada.
             }
             context.Propuesta
-            
-            
-            
+
+
+
             .AddRange(
                 new Propuesta
                 {
@@ -36,12 +36,57 @@ namespace IgnisMercado.Models
                     EstimadoDeHora = 12,
                     DescripcionDeLaPropuesta = "Se necesita camarografo con nivel avanzado para realizar un filme corto."
                 }
+
+            );
+            context.SaveChanges();
+
+
+        }
+
+        private static void SeedEmpresa(ApplicationContext context)
+        {
+            //Busca Empresas.
+            if (context.Empresa.Any())
+            {
+                return; //DB ha sido poblada.
+            }
+            context.Empresa
+
+            .AddRange(
+                new Empresa
+                {
+                    Director = "Battista Farina",
+                    Descricpion = "Firma de diseño automotriz.",
+                    Nombre = "Pininfarina",
+                    CorreoElectronico = "pininfarina@pininfarina.com",
+                    Contacto = "+9 566 8 5211"
+
+                },
+                
+                new Empresa
+                {
+                    Director = "Hideo Matsushita",
+                    Descricpion = "Empresa japonesa que diseña y fabrica micrófonos profesionales y sistemas de audio.",
+                    Nombre = "Audio Technica",
+                    CorreoElectronico = "audiotechnica@audiotechnica.com",
+                    Contacto = "+3 884 555 695"
+                },
+                
+                new Empresa
+                {
+                    Director = "Lawrence J. Ellison",
+                    Descricpion = "Empresa de desarollo de soluciones en la nube y local.",
+                    Nombre = "Oracle",
+                    CorreoElectronico = "oracle@oracle.com",
+                    Contacto = "+3 666 888 555 "
+                }
             
             );
             context.SaveChanges();
-               
 
         }
-    
-     }
+
+
+
+    }
 }
